81b3c6d2a46bcae078073e8334e008de0c95bc50
Alexander Bardashevsky <baard047@gmail.com>
2020-04-16 23:37:04 +0300

    added homepage with basic folder structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+
+import './App.css';
+
+import HomePage from "./pages/homepage/homepage";
+
+
+function App() {
+    return (
+        <>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+            </Switch>
+        </>
+    );
+}
+
+export default App;
