1916fd1f96e70fe8cc665f88007d5cd91d014458
Alexander Bardashevsky <baard047@gmail.com>
2020-04-16 23:53:38 +0300

    added withRouter routing to menu items

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import './App.css';
 
 import HomePage from "./pages/homepage/homepage";
 
+const HatsPage = () => (
+    <>
+        <h1>HATS PAGE</h1>
+    </>
+);
 
 function App() {
     return (
         <>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
+                <Route path='/shop/hats' component={HatsPage}/>
             </Switch>
         </>
     );
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -44,11 +44,8 @@ class Directory extends Component {
     };
 
     render() {
-        const menuItems = this.state.sections.map(({title, imageUrl, id, size}) => (
-            <MenuItem key={id}
-                      title={title}
-                      imageUrl={imageUrl}
-                      size={size}/>
+        const menuItems = this.state.sections.map(({id, ...other}) => (
+            <MenuItem id={id } {...other}/>
         ));
 
         return (
