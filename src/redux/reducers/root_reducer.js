12a1e7aaad6cc2535261e4759eba4f735aefc8b7
Alexander Bardashevsky <baard047@gmail.com>
2020-04-21 19:20:22 +0300

    redux-persist added to the project as an alternative way of localstorage.

--- src/redux/reducers/root_reducer.js
+++ src/redux/reducers/root_reducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage' //localstorage as default
+
 import {userReducer} from "./user_reducer";
 import {cartReducer} from "./cart_reducer";
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] //user is held by firebase
+};
+
+const rootReducer = combineReducers({
    user: userReducer,
    cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
