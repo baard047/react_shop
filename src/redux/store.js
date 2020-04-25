162e3a0b280e7726eb3bc7b994511c56e56888eb
Alexander Bardashevsky <baard047@gmail.com>
2020-04-25 12:08:58 +0300

    Redux loger working only in development enviroment

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger';
 
 import persistReducer from './reducers/root_reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 
 export const store = createStore(persistReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
